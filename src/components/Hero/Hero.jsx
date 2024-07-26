177a059d2acb6fdf2bf5196cf5434ffbee1ec150
Stormsurge01 <156475637+Stormsurge01@users.noreply.github.com>
2024-07-26 21:05:06 +0330

    motion

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,47 @@
 import BgImage from "../../assets/bg-slate.png";
-import Coffemain from "../../assets/black.png";
-import Navbar from "../Navbar/Navbar.jsx";
+import CoffeeMain from "../../assets/black.png";
+import Navbar from "../Navbar/Navbar";
+import { motion } from "framer-motion";
 const bgImage = {
     backgroundImage: `url(${BgImage})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
 };
- const Hero=()=>{
-    return(
+const Hero = () => {
+    return (
         <main style={bgImage}>
             <section className="w-full min-h-[750px]">
                 <div className="container">
-
                     <Navbar />
 
-
-
-                    {/*hero section*/}
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]">
+                    {/* hero section */}
+                    <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 place-items-center min-h-[850px]">
                         <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
-                            <h1 className="text-7xl font-bold leading-tight mr-14">بلک کافی</h1>
-                            <div className="relative">
+                            <motion.h1
+                                initial={{ opacity: 0, y: -100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 0.8,
+                                }}
+                                className="text-7xl font-bold leading-tight mr-14"
+                            >
+                                بلک کافی
+                            </motion.h1>
+                            <motion.div
+                                className="relative"
+                                initial={{ opacity: 0, y: 100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 1,
+                                }}
+                            >
                                 <div className="relative z-10 space-y-4">
                                     <h1 className="text-2xl"> عاشقان لایف استایل مشکی</h1>
                                     <h1 className="text-sm opacity-55 leading-loose">
@@ -29,39 +49,63 @@ const bgImage = {
                                         ..! اونم کنار یار!
                                     </h1>
                                 </div>
-                                <div className="absolute -top-6 -right-10 w-[250px] h-[180px] bg-gray-700/25"></div>
-                            </div>
+                                <div className=" absolute -top-6 -right-10 w-[250px] h-[180px] bg-gray-700/25"></div>
+                            </motion.div>
                         </div>
                         <div className="relative">
-                            <img src={Coffemain} alt="black coffe"
-                                 className="relative z-40 h-[400px] md:h-[700px] img-shadow"
+                            <motion.img
+                                src={CoffeeMain}
+                                alt="black coffee"
+                                className="relative z-40 h-[400px] md:h-[600px] img-shadow"
+                                initial={{ opacity: 0, scale: 0 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 100,
+                                    damping: 10,
+                                    delay: 0.4,
+                                }}
                             />
-                            <div
-                                className="h-[180px] w-[180px] absolute top-24 -left-16 border-primary border-[20px] rounded-full z-10"></div>
+                            <div className="h-[180px] w-[180px] absolute top-24 -left-16 border-primary border-[20px] rounded-full z-10"></div>
+
                             <div className="absolute -top-16 right-[60px] z-[1]">
-                                <h1 className="text-[140px] scale-150 font-bold text-darkGray/40 leading-none">
+                                <h1 className="text-[140px] scale-150 font-bold text-darkGray/50 leading-none">
                                     بلک کافی
                                 </h1>
                             </div>
                         </div>
-                        <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28 max-lg:hidden">
-                            <h1 className="opacity-0 text-7xl font-bold leading-tight mr-14">بلک کافی</h1>
+
+                        <motion.div
+                            className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28 max-lg:hidden"
+                            initial={{ opacity: 0, y: 100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 100,
+                                damping: 10,
+                                delay: 1.2,
+                            }}
+                        >
+                            <h1 className="opacity-0 text-7xl font-bold leading-tight mr-14">
+                                بلک کافی
+                            </h1>
                             <div className="relative">
                                 <div className="relative z-10 space-y-4">
-                                    <h1 className="text-2xl"> عاشقان لایف استایل مشکی</h1>
+                                    <h1 className="text-2xl"> لایف استایل مشکی</h1>
                                     <h1 className="text-sm opacity-55 leading-loose">
                                         بوی قهوه به قدری زیباست که نمیتونه متعلق به این جهان باشه
                                         ..! اونم کنار یار!
                                     </h1>
                                 </div>
-                                {/*<div className="absolute -top-6 -left-10 w-[250px] h-[180px] bg-darkGray/50"></div>*/}
+                                {/* <div className=" absolute -top-6 -left-10 w-[250px] h-[180px] bg-darkGray/50"></div> */}
                             </div>
-                        </div>
+                        </motion.div>
                         <div></div>
                     </div>
                 </div>
             </section>
         </main>
-    )
- }
-export default Hero
+    );
+};
+
+export default Hero;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,25 @@
-function Navbar() {
+import {motion} from "framer-motion";
+const Navbar = () => {
     return(
-        <div></div>
+        <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+            <div className="container">
+                <div className="flex justify-between items-center">
+                    <motion.h1
+                        initial={{opacity: 0,y: -100}}
+                        animate={{opacity: 1,y:0}}
+                        translate={{
+                            type:"spring",
+                            stiffness:100,
+                            damping:10,
+                            delay:0.2,
+                        }}
+                        className="text-2xl font-semibold">
+                        اینوا
+                        <span className="text-primary">کافی</span>
+                    </motion.h1>
+                </div>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
